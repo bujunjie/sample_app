8a8a9a68c10c873c536a0b36747f7f62700c43f6
Junjie Bu <bujunjie@gmail.com>
2010-12-02 14:58:26 -0800

    Done with user sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 SampleApp::Application.routes.draw do
+
 #  get "users/new"
   resources :users
+  
+  resources :sessions, :only => [:new, :create, :destroy]
+  
 
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+  
+  
   root :to => 'pages#home'
   
   get "pages/home"
